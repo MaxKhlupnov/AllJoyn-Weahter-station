d46ce2f3c21288a6f232af990f20d37f0cc34ee6
Maxim Khlupnov <m.khlupnov@inbox.ru>
2015-09-08 17:49:49 +0300

    Append client components

---
+++ SensorClient/SensorClient/DataModel/TemperatureSensor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.mtcmoscow.SensorHub.Temperature;
+
+namespace SensorClient.DataModel
+{
+    public enum TemperatureUnits { Celsius, Fahrenheits };
+    public class TemperatureSensor : ConnectTheDotsSensor
+    {
+        private TemperatureUnits _units;
+        public TemperatureUnits Units
+        {
+            get
+            {
+                return this._units;
+            }
+            set
+            {
+                unitofmeasure = value.ToString();
+                this._units = value;
+            }
+        }
+
+        private TemperatureConsumer consumer;
+        public TemperatureSensor(TemperatureConsumer consumer, string UniqueName)
+        {
+            base.guid = UniqueName;
+            base.measurename = "Temperature";
+            this.consumer = consumer;
+            this.consumer.SessionLost += Consumer_SessionLost;
+            this.Units = TemperatureUnits.Celsius;
+        }
+
+        private void Consumer_SessionLost(TemperatureConsumer sender, Windows.Devices.AllJoyn.AllJoynSessionLostEventArgs args)
+        {
+            ///TODO: Do something if we lost connection with sensor
+        }
+
+
+        public async Task<int> ReadDataAsync()
+        {
+            base.timecreated = DateTimeOffset.Now.ToLocalTime().ToString();
+
+            if (this.Units == TemperatureUnits.Celsius)
+            {
+                TemperatureGetCelsiusResult cResults =   await this.consumer.GetCelsiusAsync();
+                base.value = cResults.Celsius;
+                return cResults.Status;
+            }else if (this.Units == TemperatureUnits.Fahrenheits)
+            {
+                TemperatureGetFahrenheitsResult fResult = await this.consumer.GetFahrenheitsAsync();
+                base.value = fResult.Fahrenheits;
+                return fResult.Status;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ SensorClient/SensorClient/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading;
+
+
+using SensorClient.DataModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SensorClient
+{
+
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private WeatherShieldViewModel ShieldViewModel = new WeatherShieldViewModel();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = ShieldViewModel;
+        }
+
+        private void Status_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}
